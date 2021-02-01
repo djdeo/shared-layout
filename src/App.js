a172f4b97a00b0456603c2fbf42c1beabf8755e2
djdeo <deoseoer@gmail.com>
2021-02-01 15:26:37 +0800

    feat: add expand effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
+import { useState } from "react";
+
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <AnimateSharedLayout>
+      <motion.ul layout initial={{borderRadius:25}}><Item /> </motion.ul>
+    </AnimateSharedLayout>
   );
 }
 
 export default App;
+
+const Item = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleOpen = () => setIsOpen(!isOpen)
+
+  return (
+    <motion.li onClick={toggleOpen} initial={{borderRadius:10}}>
+      <motion.div className="avatar" layout />
+      <AnimatePresence>{isOpen && <Content />}</AnimatePresence>
+    </motion.li>
+  )
+}
+
+
+const Content = () => (
+  <motion.div layout initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+    <div className="row" />
+    <div className="row" />
+    <div className="row" />
+  </motion.div>
+)
